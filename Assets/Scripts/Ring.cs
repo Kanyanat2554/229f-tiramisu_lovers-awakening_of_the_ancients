5e546c717777caa00e6fb526a79b51ad5ddd5ed8
Kanyanat Sermsup <kanyanat.serm@bumail.net>
2025-05-05 03:37:29 +0700

    ทำให้เก็บสมบัติได้

---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    public static int collectedRings = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collectedRings++;
+            Debug.Log($"Rings collected: {collectedRings}");
+
+            Destroy(gameObject);
+
+            if (collectedRings >= 3)
+            {
+                RemoveBarrier();
+            }
+        }
+    }
+
+    private void RemoveBarrier()
+    {
+        GameObject barrier = GameObject.FindGameObjectWithTag("Barrier");
+        if (barrier != null)
+        {
+            Destroy(barrier);
+            Debug.Log("Barrier Removed!");
+        }
+    }
+}
