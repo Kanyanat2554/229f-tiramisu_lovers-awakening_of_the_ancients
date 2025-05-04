a84fc116ed1dee243be51bb70af41a11de6cdddd
Kanyanat Sermsup <kanyanat.serm@bumail.net>
2025-05-05 00:49:18 +0700

    เพิ่มให้กด Shift เพื่อวิ่ง

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed;
+    public float RunMultiplier = 1.5f;
 
     Vector2 moveInput;
 
     float move;
+    private bool isRunning;
 
     public float JumpForce;
     public bool IsJumping;
 
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
+    public Transform GroundCheck;
+    public float GroundCheckRadius = 0.2f;
+    public LayerMask GroundLayer;
+
+    //Stamina System
+    public float Stamina = 100f;
+    public float MaxStamina = 100f;
+    public float StaminaDrainRate = 20f;
+    public float StaminaRecoverRate = 10f;
 
     Rigidbody2D rb2d;
 
@@ -30,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
         /*moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         rb2d.AddForce(moveInput * Speed);*/
 
-        IsJumping = !Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        /*IsJumping = !Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         //use rigidbody2d to move left and right (x-axis)
         move = Input.GetAxis("Horizontal"); //x - axis
@@ -43,7 +51,38 @@ public class PlayerMovement : MonoBehaviour
             rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, JumpForce);
 
             Debug.Log("Jump");
+        }*/
+
+        bool grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayer);
+        IsJumping = !grounded;
+
+        move = Input.GetAxisRaw("Horizontal"); 
+
+        // กด Shift เพื่อวิ่ง
+        isRunning = Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && Stamina > 0f;
+
+        float currentSpeed = Speed;
+
+        if (isRunning)
+        {
+            currentSpeed *= RunMultiplier;
+            Stamina -= StaminaDrainRate * Time.deltaTime;
+            Stamina = Mathf.Clamp(Stamina, 0f, MaxStamina);
         }
+        else
+        {
+            Stamina += StaminaRecoverRate * Time.deltaTime;
+            Stamina = Mathf.Clamp(Stamina, 0f, MaxStamina);
+        }
+
+        rb2d.linearVelocity = new Vector2(move * currentSpeed, rb2d.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && !IsJumping)
+        {
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, JumpForce);
+        }
+
+        Debug.Log("Stamina: " + Stamina);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
