425efd56ed5c89b6751c5c81025bad95fcdd93c6
Kanyanat Sermsup <kanyanat.serm@bumail.net>
2025-05-05 05:16:23 +0700

    ทำให้ Mummy สามารถยิงได้

---
+++ Assets/Scripts/MummyBullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MummyBullet : MonoBehaviour
+{
+    public float lifetime = 2f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+   
+}
---
+++ Assets/Scripts/MummyLevelOne.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class MummyLevelOne : MonoBehaviour
+{
+    public GameObject mummyBulletPrefab;  
+    public Transform shootPoint;             
+    public float shootInterval = 1f;    
+    public float projectileSpeed = 10f;  
+
+    private Transform target;
+    private bool isPlayerInRange = false;
+    private float shootTimer = 0f;
+
+    private void Update()
+    {
+        if (isPlayerInRange && target != null)
+        {
+            shootTimer += Time.deltaTime;
+
+            if (shootTimer >= shootInterval)
+            {
+                ShootPlayer();
+                shootTimer = 0f;
+            }
+        }
+    }
+
+    private void ShootPlayer()
+    {
+        //create bullet
+        GameObject projectile = Instantiate(mummyBulletPrefab, shootPoint.position, Quaternion.identity);
+
+        //calculate direction to player
+        Vector2 direction = (target.position - shootPoint.position).normalized;
+
+        //add speed to bullet
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = direction * projectileSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            target = collision.transform;
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            target = null;
+            shootTimer = 0f;
+        }
+    }
+}
