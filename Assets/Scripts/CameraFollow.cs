28abb9a9718fe1376f9435d6c6045becce7c7129
Kanyanat Sermsup <kanyanat.serm@bumail.net>
2025-05-05 02:29:11 +0700

    ทำให้กล้องตาม Player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;  
-    public Vector3 offset;    
-    public float smoothSpeed = 0.125f;  
+    public Transform player;
+    private Vector3 offset = new Vector3(0, 0, -10);
+    public float smoothSpeed = 0.125f; 
 
     private void LateUpdate()
     {
         if (player != null)
         {
             Vector3 desiredPosition = player.position + offset;
-            
+
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
         }
